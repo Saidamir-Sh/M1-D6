fd59e28dca591e24771dd9aecbaf69e9de863f5c
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2021-11-01 01:11:32 +0100

    solved basic taks

--- M1-D6.js
+++ M1-D6.js
@@ -14,35 +14,72 @@ ASSIGNMENT RULES
 /* EXERCISE A
    Create a variable called test and assign a string value to it.
 */
+let test = "Striver"
 
 /* EXERCISE B
     Create a variable called sum and assign to it the result of the sum between the numbers 10 and 20.
 */
 
+const sumBetweenNums = function () {
+  let sum = 0
+  for (let i = 10; i < 20; i++) {
+     sum += i
+  }
+  console.log(sum)
+}
+sumBetweenNums()
+
 /* EXERCISE C
     Create a variable called random and assign to it a random number between 0 and 20 (it should be randomly created at each execution).
 */
+const randomNumber = function () {
+  let random = Math.floor(Math.random() *20)
+  console.log(random)
+}
+
+randomNumber()
 
 /* EXERCISE D
     Create a variable called me and assign to it an object containing the following information: name = your name, surname = your surname, age = your age.
 */
+let me = {
+  name : "Saidamir",
+  surname : "Sharofuddinov",
+  age : 20
+}
 
+console.log(me)
 /* EXERCISE E
     Write a piece of code for programmatically removing the age property from the previously create object.
 */
+delete me.age
+console.log(me)
 
 /* EXERCISE F
    Write a piece of code for programmatically adding to the me object you defined before an array called skills, containing the programming languages you know right now.
 */
 
+me.skills = ["JavaScript", "TypeScript", "C++"]
+console.log(me)
+
 /* EXERCISE G
    Write a piece of code for programmatically removing the last skill from the skills array inside the me object.
 */
+me.skills.pop()
+console.log(me)
 
 // JS Functions
 /* EXERCISE 1
     Write a function called dice; it should randomize an integer number between 1 and 6.
 */
+
+const dice = function () {
+  
+  let randomInteger = Math.floor(Math.random() * 6) + 1 
+  console.log(randomInteger)
+
+}
+dice()
 
 /* EXERCISE 2
     Write a function called whoIsBigger which receives 2 numbers as parameters and returns the biggest one.
